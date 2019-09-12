1b14bc1766c1cd49172720ad823f2669383eb17b
Luiserebii <luis@serebii.io>
2019-09-12 17:47:17 -0400

    Adding .gitignore and beginnings of small Fibonacci program

---
+++ strings/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::string;
+using std::stringstream;
+
+int main() {
+
+    cout << "Hello, friend! :)" << endl;
+    cout << "Please type in a number representing the upper limit of Fibonacci numbers you are looking for today." << endl;
+
+    int limit;
+    cin >> limit;
+
+    cout << "Your limit: " << limit << endl;    
+    return 0;
+}
+
+
+
+
+
