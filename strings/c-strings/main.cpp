155f33c040d0453906ed83b512e5b9f0c31f334b
Luiserebii <luis@serebii.io>
2019-09-12 20:52:05 -0400

    Idea to make l337code thing here

---
+++ strings/c-strings/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+char[] tol337c0de() {
+    if()
+}
+
+char[] dec0de() {
+
+}
+
+int main() {
+
+    char cstring[3]; 
+    cout << "Type in a tiny word of three characters for our little c-string: " << endl;
+
+    cin >> cstring;
+    cout << "cstring:   " << cstring << endl << "LENGTH:  " << strlen(cstring) << endl;
+
+    return 0; 
+}
+
+
