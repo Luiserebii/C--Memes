7e77b004dd615357c9808ff0cd177aa03f8a6d25
Luiserebii <luis@serebii.io>
2019-09-16 17:22:20 +0000

    Messing around by running each native string function in a convenient litle script

---
+++ strings/basic-functions/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * The idea of this file is just to play with using native C++ string functions
+ * 
+ *
+ **/
+
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+
+
+
+
+}
+
+void runSize() {
+    cout << "Please enter a string." << endl;
+    string str;
+    cin >> str;
+    cout << "The string you entered is: " << str << endl << endl;
+    cout << "size(): " << str.size() << endl;
+}
+
