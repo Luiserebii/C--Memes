7023df016c35927df8879aa2cdfba7941558f45e
Luiserebii <luis@serebii.io>
2019-09-23 23:24:40 +0000

    Attempt at making abstract class

--- strings/basic-functions/prompter.cpp
+++ strings/basic-functions/prompter.cpp
@@ -1,3 +1,22 @@
 #include <string>
 
+class Prompter {
 
+    private:
+        Option[] options;
+
+
+    public:
+        //This is how to create an abstract method, I think?
+        //Uh oh, issue here... We
+        virtual Option[] getOptions() = 0;
+        virtual void setOptions(Option[] opts = 0);
+        
+
+
+}
+
+struct Option {
+    int id;
+    string name;
+}
