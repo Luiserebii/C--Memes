e5caa55e288ef6f8476c6be8278b00da0f00425a
Luiserebii <luis@serebii.io>
2019-09-27 02:31:22 -0400

    Awesome, integrated this refactor! Will continue

--- data-structures/vectorvslist/main.cpp
+++ data-structures/vectorvslist/main.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <list>
 
+#include "time.h"
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -22,18 +24,18 @@ using std::list;
 
 using std::chrono::high_resolution_clock;
 using std::chrono::time_point;
-
+/*
 std::ostream& printTimeDifference(std::ostream& out, std::chrono::high_resolution_clock::time_point a, std::chrono::high_resolution_clock::time_point b, std::string prefix = "", std::string postfix = "");
 std::string timeDifferenceToString(std::chrono::high_resolution_clock::time_point a, std::chrono::high_resolution_clock::time_point b);
-std::string toTitle(std::string str);
+*/std::string toTitle(std::string str);
 
 void vectorInsertDelete(std::vector<int>& v);
 void listInsertDelete(std::list<int>& l);
 
-struct TimeRecord {
+/*struct TimeRecord {
     std::chrono::high_resolution_clock::time_point start;
     std::chrono::high_resolution_clock::time_point end;
-};
+};*/
 
 /**
  * Exercise:
@@ -143,25 +145,6 @@ void listInsertDelete(list<int>& l) {
     cout << "[TESTERS NOTE]: The sizes should be the same, as each position is 1 opportunity for an insert/delete" << endl;
     */
 
-}
-
-ostream& printTimeDifference(ostream& out, high_resolution_clock::time_point a, high_resolution_clock::time_point b, string prefix, string postfix) {
-    if(prefix != "") out << prefix;
-    out << timeDifferenceToString(a, b) << endl;
-    if(postfix != "") out << postfix;
-    return out;
-}
-
-string timeDifferenceToString(high_resolution_clock::time_point a, high_resolution_clock::time_point b) {
-    stringstream ss;
-//    high_resolution_clock::time_point diff = b - a;
-    ss << "Elapsed time: " << endl << endl
-        << std::chrono::duration_cast<std::chrono::nanoseconds>(b - a).count() << " ns" << endl
-        << std::chrono::duration_cast<std::chrono::microseconds>(b - a).count() << " µs" << endl
-        << std::chrono::duration_cast<std::chrono::milliseconds>(b - a).count() << " ms" << endl
-        << std::chrono::duration_cast<std::chrono::seconds>(b - a).count() << " s" << endl
-    ;
-    return ss.str();
 }
 
 string toTitle(string str) {
