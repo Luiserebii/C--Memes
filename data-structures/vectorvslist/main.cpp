5026f4890ef198a353c58975ecfe77bcfbadfe1f
Luiserebii <luis@serebii.io>
2019-09-26 22:35:01 -0400

    Created exercise on testing and benchmarking vectors vs lists

---
+++ data-structures/vectorvslist/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <chrono>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
+
+/**
+ * Exercise:
+ * 
+ * Attempt to absorb a data set of integers via a file, load them into a vector and list.
+ * Within both data structures, add a random number of random integers (from 1 - 1/2 size of list)
+ * to random spots, using insert(). Compare by benchmarking the time.
+ */
+int main() {
+
+    const time_point start = high_resolution_clock::now();
+
+    console.log(start);
+    return 0;
+}
