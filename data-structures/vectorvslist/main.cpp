b86625c04a06575b2bee6a962cdb1722d4f92f23
Luiserebii <luis@serebii.io>
2019-09-27 00:22:51 -0400

    Attempting to use example here: http://www.cplusplus.com/reference/chrono/high_resolution_clock/now/

--- data-structures/vectorvslist/main.cpp
+++ data-structures/vectorvslist/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <sstream>
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::stringstream;
+using std::istream;
 
 using std::chrono::high_resolution_clock;
 using std::chrono::time_point;
@@ -16,27 +21,30 @@ using std::chrono::time_point;
  */
 int main() {
 
-    const time_point<high_resolution_clock> start = high_resolution_clock::now();
+//    const time_point<high_resolution_clock> start = high_resolution_clock::now();
 
-    cout << "The current time: " << start << endl;
 
-    const time_point<high_resolution_clock> end = high_resolution_clock::now();
+//    const time_point<high_resolution_clock> end = high_resolution_clock::now();
 
+    const high_resolution_clock::time_point start = high_resolution_clock::now();
+    const high_resolution_clock::time_point end = high_resolution_clock::now();
     return 0;
 }
 
-istream& printTimeDifference(istream& in, time_point<high_resolution_clock> a, time_point<high_resolution_clock> b) {
+istream& printTimeDifference(istream& in, /*time_point<high_resolution_clock> a, time_point<high_resolution_clock> b*/ high_resolution_clock::time_point a, high_resolution_clock::time_point b) {
     in << timeDifferenceToString(a, b);
     return in;
 }
 
-string timeDifferenceToString(time_point<high_resolution_clock> a, time_point<high_resolution_clock> b) {
+string timeDifferenceToString(/*time_point<high_resolution_clock> a, time_point<high_resolution_clock> b*/ high_resolution_clock::time_point a, high_resolution_clock::time_point b) {
     stringstream ss;
+//    time_point<high_resolution_clock> diff = b - a;
+    high_resolution_clock::time_point diff = b - a;
     in << "Elapsed time: " << endl << endl
-        << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " ns" << endl
-        << chrono::duration_cast<chrono::microseconds>(end - start).count() << " µs" << endl
-        << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl
-        << chrono::duration_cast<chrono::seconds>(end - start).count() << " s" << endl
+        << std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count() << " ns" << endl
+        << std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << " µs" << endl
+        << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() << " ms" << endl
+        << std::chrono::duration_cast<std::chrono::seconds>(diff).count() << " s" << endl
     ;
     return ss.str();
 }
