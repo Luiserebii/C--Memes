f3c177a04b599d636a5317e84f9b24ed8787e37d
Luiserebii <luis@serebii.io>
2019-09-27 20:43:44 -0400

    Implementing vectorToString function for convenience

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <ctime>
 
+#include <sstream>
+#include <string>
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -13,8 +16,9 @@ int main() {
 
     srand(time(NULL));
 
-
-
+    vector<int> a, b;
+    fillInt(a, 10);
+    fillInt(b, 15);
     return 0;
 }
 
@@ -22,4 +26,14 @@ void fillInt(vector<int>& v, vector<int>::size_type s) {
     for(vector<int>::const_iterator i = v.begin(); i < s; ++i) {
         v.push_back(rand());
     }
+}
+
+string vectorToString(const vector<int>& v) {
+    stringstream ss;
+    for(vector<int>::const_iterator i = v.begin(); i < --v.end(); ++i) {
+        ss << *i << " ";
+    }
+    ss << *--v.end();
+
+    return ss.str();
 }
