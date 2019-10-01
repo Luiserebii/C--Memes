6f7296b36d312fc27098c3a3b161d95bf49cd05f
Luiserebii <luis@serebii.io>
2019-10-01 01:23:08 -0400

    Implemented wordMapToString, fleshed some stuff out, added code to test

--- data-structures/map/main.cpp
+++ data-structures/map/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <map>
 
 using std::cin;
@@ -6,10 +7,16 @@ using std::cout;
 using std::endl;
 
 using std::map;
+using std::string;
+
+void createMap(map<int, string>& m);
+string wordMapToString(const map<int, string>& m);
 
 int main() {
-
-
+    map<int, string> m;
+    createMap(m);
+    string mapStr = wordMapToString(m);
+    cout << "Map: " << mapStr << endl;
 }
 
 void createMap(map<int, string>& m) {
@@ -21,4 +28,13 @@ void createMap(map<int, string>& m) {
     m[5] = "map!";
 }
 
-string wordMapToString(const map<>)
+//This is a minor issue in that the last fragment of the string will have
+//an extra space, but I am willing to ignore this for the sake of the
+//example for the time being
+string wordMapToString(const map<int, string>& m) {
+    string s = "";
+    for(map<int, string>::const_iterator i = m.begin(); i != m.end(); ++i) {
+        s += i->second + " ";
+    }
+    return s;
+}
