44b29442bd95e09c524843a0d7d22ecef857630a
Luiserebii <luis@serebii.io>
2019-10-01 01:16:30 -0400

    Beginning to work on playing with maps

---
+++ data-structures/map/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::map;
+
+int main() {
+
+
+}
+
+void createMap(map<int, string>& m) {
+    m[0] = "Hello";
+    m[1] = "I";
+    m[2] = "am";
+    m[3] = "a";
+    m[4] = "word";
+    m[5] = "map!";
+}
+
+string wordMapToString(const map<>)
