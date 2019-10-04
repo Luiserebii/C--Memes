f0c0cabd884d8bf641da50f94cdba346c4e9b29a
Luiserebii <luis@serebii.io>
2019-10-04 02:20:45 -0400

    Writing out getters and basic constructor

--- classes/basic/main.cpp
+++ classes/basic/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
+
+using std::string;
 
 class Book {
     
@@ -12,7 +15,9 @@ class Book {
         string description;
 
     public: 
-
+        
+        Book();
+ 
         void setTitle();
         void setAuthor();
         void setDescription();
@@ -21,6 +26,29 @@ class Book {
         string getAuthor();
         string getDescription();
 
+        string toString();
+}
+
+Book::Book() {
+    title = "Sample Title";
+    author = "Sample Author";
+    description = "Sample Description";
+}
+
+string Book::getTitle() {
+    return title;
+}
+
+string Book::getAuthor() {
+    return author;
+}
+
+string Book::getDescription() {
+    return description;
+}
+
+string Book::toString() {
+    
 }
 
 int main() {
