fe009d0c41dd3a83faf0d1254af5ee071e8459f8
Luiserebii <luis@serebii.io>
2019-10-04 02:37:10 -0400

    Splitting main.cpp into book.cpp and book.h

--- classes/basic/main.cpp
+++ classes/basic/main.cpp
@@ -1,74 +1,9 @@
 #include <iostream>
-#include <string>
+#include "book.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
-
-using std::string;
-
-class Book {
-    
-    private:
-        string title;
-        string author;
-        string description;
-
-    public: 
-        
-        Book();
-        Book(const string& t, const string& a, const string& d);
-
-        string getTitle();
-        string getAuthor();
-        string getDescription();
-
-        void setTitle(const string& t);
-        void setAuthor(const string& a);
-        void setDescription(const string& d);
-
-        string toString();
-};
-
-Book::Book() {
-    title = "Sample Title";
-    author = "Sample Author";
-    description = "Sample Description";
-}
-
-Book::Book(const string& t, const string& a, const string& d) {
-    setTitle(t);
-    setAuthor(a);
-    setDescription(d);
-}
-
-string Book::getTitle() {
-    return title;
-}
-
-string Book::getAuthor() {
-    return author;
-}
-
-string Book::getDescription() {
-    return description;
-}
-
-void Book::setTitle(const string& t) {
-    title = t;
-}
-
-void Book::setAuthor(const string& a) {
-    author = a;
-}
-
-void Book::setDescription(const string& d) {
-    description = d;
-}
-
-string Book::toString() {
-    return title + "\n" + author + "\n" + description;
-}
 
 int main() {
     Book b;    
