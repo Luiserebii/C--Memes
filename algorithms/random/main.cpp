0de19e1dea405bf07744eaf1405d0373ec0bc7f1
Luiserebii <luis@serebii.io>
2019-10-05 22:13:08 -0400

    Fleshing out main body of the program

--- algorithms/random/main.cpp
+++ algorithms/random/main.cpp
@@ -12,7 +12,15 @@ void printVector(const vector<int>& v, ostream& out);
 int main() {
     
     vector<int> meme = {1, 2, 6, 8, 10, 7, 9};
+    cout << "Current vector:" << endl;
+    printVector(meme, cout);
 
+    //Partition all the ones that aren't even, in this order
+    partition(meme.start(), meme.end(), isEven);
+
+    //Print post-partition
+    cout << "Vector post-partition: partition(meme.start(), meme.end(), isEven)" << endl;
+    printVector(meme, cout);
     return 0;
 }
 
