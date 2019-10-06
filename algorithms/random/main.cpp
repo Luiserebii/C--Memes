ff0a20195b0e57520a29f7723c226f13813f3331
Luiserebii <luis@serebii.io>
2019-10-05 22:09:08 -0400

    Adding printVector function

--- algorithms/random/main.cpp
+++ algorithms/random/main.cpp
@@ -2,16 +2,26 @@
 #include <vector>
 
 using std::vector;
+using std::ostream;
+using std::cout;
+using std::endl;
 
-bool isEven(double d);
+bool isEven(int d);
+void printVector(const vector<int>& v, ostream& out);
 
 int main() {
     
-    vector<double> meme = {1,2.3,6,8,10,7.9};
+    vector<int> meme = {1, 2, 6, 8, 10, 7, 9};
 
     return 0;
 }
 
-bool isEven(double d) {
+bool isEven(int d) {
     return ((d % 2) == 0);
+}
+
+void printVector(const vector<int>& v, ostream& out) {
+    for(vector<int>::const_iterator i = v.begin(); i != v.end(); ++i) {
+        out << *i << " ";
+    }
 }
