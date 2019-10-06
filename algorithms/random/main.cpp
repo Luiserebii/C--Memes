361089c5b37d700be09ab119f551b3e9f042d4d1
Luiserebii <luis@serebii.io>
2019-10-05 22:20:08 -0400

    Added stable_partition usage

--- algorithms/random/main.cpp
+++ algorithms/random/main.cpp
@@ -8,6 +8,7 @@ using std::cout;
 using std::endl;
 
 using std::partition;
+using std::stable_partition;
 
 bool isEven(int d);
 void printVector(const vector<int>& v, ostream& out, bool headline = true);
@@ -15,15 +16,23 @@ void printVector(const vector<int>& v, ostream& out, bool headline = true);
 int main() {
     
     vector<int> meme = {1, 2, 6, 8, 10, 7, 9};
+    vector<int> memeCopy = meme;
     cout << "Current vector:" << endl;
     printVector(meme, cout);
 
-    //Partition all the ones that aren't even, in this order
+    //Partition all the ones that aren't even
     partition(meme.begin(), meme.end(), isEven);
 
     //Print post-partition
     cout << "Vector post-partition: partition(meme.start(), meme.end(), isEven)" << endl;
     printVector(meme, cout);
+
+    //Partition all the ones that aren't even, in order
+    stable_partition(memeCopy.begin(), memeCopy.end(), isEven);
+
+    //Print post-stable_partition
+    cout << "Vector post-stable_partition: stable_partition(memeCopy.begin(), memeCopy.end(), isEven)" << endl;
+    printVector(memeCopy, cout);
     return 0;
 }
 
