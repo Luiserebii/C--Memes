30856da194d495ab95a0316d6ab4a44f5083248e
Luiserebii <luis@serebii.io>
2019-10-05 22:00:48 -0400

    Adding a random program file to run standard library algs on

---
+++ algorithms/random/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+bool isEven(double d)
+
+int main() {
+    
+    vector<double> meme = {1,2.3,6,8,10,7.9};
+
+    return 0;
+}
+
+bool isEven(double d) {
+    return ((d % 2) == 0);
+}
