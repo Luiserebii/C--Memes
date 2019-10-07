72f9fa5c6d6a503d0bed614eb4944572a3f3b882
Luiserebii <luis@serebii.io>
2019-10-06 22:37:56 -0400

    Curious... this runs, and we get it back, but we get 2 as integer...

--- templates/two-types/main.cpp
+++ templates/two-types/main.cpp
@@ -6,6 +6,11 @@ using std::endl;
 template <class T>
 T testWithTwoTypes(const T& left, const T& right);
 
+template <class T, class U>
+T testWithTwoTypesToo(const T& left, const U& right);
+
+template <class T, class U>
+T max(const T& left, const U& right);
 
 int main() {
 
@@ -14,10 +19,25 @@ int main() {
     //This will break, as function def had specified two of the same var
     //cout << testWithTwoTypes(x, y) << endl;
 
+    //This, however, does of course run:
+    cout << testWithTwoTypesToo(x, y) << endl;
+
+    //What if we took advantage of implicit type comparisons?
+    cout << max(x, y) << endl;
     return 0;
 }
 
 template <class T>
 T testWithTwoTypes(const T& left, const T& right) {
-        return left;
+    return left;
+}
+
+template <class T, class U>
+T testWithTwoTypesToo(const T& left, const U& right) {
+    return left;
+}
+
+template <class T, class U>
+T max(const T& left, const U& right) {
+    return left > right ? left : right;
 }
