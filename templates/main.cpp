92fc472a4491d533502af3f402fa0f7d10e950ee
Luiserebii <luis@serebii.io>
2019-10-06 21:43:02 -0400

    Managed to use it to add some basic stuff! I received a really interesting error when trying to use it for add two strings:
    main.cpp: In instantiation of ‘T add(T, T) [with T = const char*]’:
    main.cpp:19:50:   required from here
    main.cpp:33:15: error: invalid operands of types ‘const char*’ and
    ‘const char*’ to binary ‘operator+’
         return v1 + v2;
    
    I realize that string literals are really just "c-strings", which I
    suppose are really just char[]s, so I ended up just passing an
    intialized string object off of the literal, and it's running!

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 template <class T>
 T returnVar(T val);
 
+template <class T>
+T add(T v1, T v2);
+
 int main() {
 
     int meme = returnVar(12);
-    cout << "AAAAA: " << meme << endl;
+    cout << "Template return: " << meme << endl;
+    cout << "Template adding: " << endl;
+    cout << "   " << add(1, 2) << endl;
+    cout << "   " << add(1.2, 2.5) << endl;
+    cout << "   " << add(string("Fly me to "), string("the moon")) << endl;
+    cout << "   " << add(-5, 3) << endl;
+
+
     return 0;
 }
 
-template <class T> T returnVar(T val){
+template <class T> 
+T returnVar(T val) {
     return val;
+}
+
+template <class T>
+T add(T v1, T v2) {
+    return v1 + v2;
 }
