0402027859f2a350d06e72aac675d5fff3c43401
Luiserebii <luis@serebii.io>
2019-10-30 22:04:31 -0400

    Fleshing out basic Node class

--- pointers/memelist/linkedlist.cpp
+++ pointers/memelist/linkedlist.cpp
@@ -7,9 +7,21 @@ class Node {
         T val; //Our value
         Node* next; //Pointer to next node
     public:
-        
+        Node(T v);
+        Node(T v, Node* n);        
 
+};
 
+template <class T>
+Node<T>::Node(T v) {
+    val = v;
+    next = NULL;
+}
+
+template <class T>
+Node<T>::Node(T v, Node* n) {
+    val = v;
+    next = n;
 }
 
 int main() {
