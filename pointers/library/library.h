893daef30a5938c565e00f7f45345b66ceddd321
Luiserebii <luis@serebii.io>
2019-10-31 23:11:38 +0000

    Beginning small Library class using internal array, very basic

---
+++ pointers/library/library.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_LIBRARY_H
+#define GUARD_LIBRARY_H
+#define CAPACITY 10
+
+#include "book.h"
+#include <iostream>
+#include <string>
+
+class Library {
+    private:
+        Book books[CAPACITY];
+    public:
+        Library();
+
+        int addBook(const Book b);
+        std::string toString();
+        std::ostream write();
+        void output();
+
+
+
+};
+
+#endif
