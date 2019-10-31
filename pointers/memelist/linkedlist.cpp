9dd12e4d7780ca271948a09a7bd369453c1ccc21
Luiserebii <luis@serebii.io>
2019-10-30 22:20:51 -0400

    Fleshing out LinkedList

--- pointers/memelist/linkedlist.cpp
+++ pointers/memelist/linkedlist.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
 #include "node.h"
 
+template <class T>
+class LinkedList {
+    private:
+        Node<T>* head;
+        
+    public:
+        LinkedList();
+
+        Node<T>* getHead() const;
+        void setHead(Node<T> n);
+
+
+};
+
+template <class T>
+LinkedList<T>::LinkedList(): head(nullptr) {
+    
+}
+
+template <class T>
+Node<T>* LinkedList<T>::getHead() const {
+    return head;
+}
+
+template <class T>
+void LinkedList<T>::setHead(Node<T>* n) {
+    head = h;
+}
 
 int main() {
 
