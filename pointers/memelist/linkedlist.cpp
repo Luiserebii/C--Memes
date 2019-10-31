898810e7dda0acf8062cdc40594eb58b72d54715
Luiserebii <luis@serebii.io>
2019-10-30 21:58:52 -0400

    Fleshing out basics of noob linkedlist from memory

---
+++ pointers/memelist/linkedlist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+template <class T>
+class Node {
+    private:
+        T val; //Our value
+        Node* next; //Pointer to next node
+    public:
+        
+
+
+}
+
+int main() {
+
+
+
+}
