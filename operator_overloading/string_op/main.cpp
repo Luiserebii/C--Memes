82099fb765251a3a1488f8c788c6bf2a8dc099b2
Luiserebii <luis@serebii.io>
2019-11-05 20:32:37 -0500

    Implementing operator- and fixing some compilation errors

--- operator_overloading/string_op/main.cpp
+++ operator_overloading/string_op/main.cpp
@@ -17,12 +17,26 @@ class String {
         // * operator "multiplies" the internal string by the integer passed in second operand
         // / operator "divides" the internal string by the integer passed in the second operand
         // [] operator obtains a char from the string
-        String operator+(const String string);
-
+        String operator+(const String stringO);
+        String operator-(const String stringO);
 };
 
-String String::operator+(const String string) {
-    return String(s + string.getString());
+String String::operator+(const String stringO) {
+    return String(s + stringO.getString());
+}
+
+String String::operator-(const String stringO) {
+    string str = "";
+    int pos = s.find(stringO.getString());
+    if(pos != -1) {
+        //Get first piece without
+        str += s.substr(0, pos);
+        //Get second piece without
+        str += s.substr(pos + (stringO.getString()).length);
+        return str;
+    } 
+    //If we hit here, just return what we have
+    return s;
 }
 
 int main() {
