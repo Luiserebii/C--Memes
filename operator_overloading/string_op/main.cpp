057054dde6791c0935b53be1cfa64cc3f8c78417
Luiserebii <luis@serebii.io>
2019-11-05 20:50:26 -0500

    Refactoring output of String to print() function

--- operator_overloading/string_op/main.cpp
+++ operator_overloading/string_op/main.cpp
@@ -2,18 +2,35 @@
 #include <iostream>
 #include <string>
 
+using std::ostream;
 using std::cout;
 using std::endl;
 using std::string;
+
+ostream& print(const String& sO, const string& name);
 
 int main() {
 
     String s = String("uguu");
     String t = String("owo");
 
-    cout << "String s: " << s.str() << endl;
-    cout << "String t: " << t.str() << endl;
+//    cout << "String s: " << s.str() << endl;
+//    cout << "String t: " << t.str() << endl;
+    print(s, "s");
+    print(t, "t");
+
+    //operator+
     String sum = s + t;
     cout << "s + t: " << sum.str() << endl; 
 
+    //operator-
+
+//    String diff = String("Fly me to the moon") - String("me to the ");
+//    cout << ""
+
+}
+
+ostream& print(const String& sO, const string& name) {
+    cout << "String " << name << ": " << sO.str() << endl;
+    return cout;
 }
