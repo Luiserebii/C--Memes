b3806270eba1aa1d7a54a2adb1a5a7dc5479ed31
Luiserebii <luis@serebii.io>
2019-11-05 20:13:54 -0500

    Outlining operator overload implementation details

---
+++ operator_overloading/string_op/main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class String {
+
+    private: 
+        string s;
+    public:
+        string getString() const { return s; }
+        void setString(const string str) { s = str; }
+
+        // + operator concatenates internal strings together
+        // - operator removes string, if existing
+        // * operator "multiplies" the internal string by the integer passed in second operand
+        // / operator "divides" the internal string by the integer passed in the second operand
+        // [] operator obtains a char from the string
+        String operator+(const String string);
+
+}
+
+int main() {
+
+
+}
