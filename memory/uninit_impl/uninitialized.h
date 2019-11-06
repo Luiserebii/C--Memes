9efebee5def12a24bf116867b3d29a0a0b9b3fef
Luiserebii <luis@serebii.io>
2019-11-06 16:53:00 -0500

    Both functions implemented, now to test

--- memory/uninit_impl/uninitialized.h
+++ memory/uninit_impl/uninitialized.h
@@ -3,17 +3,26 @@
 
 #include <memory>
 
-template <class T, In>
+template <class T, class In>
 T* uninitialized_copy(In begin, In end, T* out) {
 
     std::allocator<T> alloc;
     while(begin != end) {
-        alloc.construct(out++, *begin++)
+        alloc.construct(out++, *begin++);
     }
     return out;
 
 }
 
+template <class T> 
+void uninitialized_fill(T* begin, T* end, const T& val) {
+    
+    std::allocator<T> alloc;
+    while(begin != end) {
+        alloc.construct(begin++, val);
+    }
+
+}
 
 
 #endif
