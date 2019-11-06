96decf52cffbe19c3d118862f416152ec7d0f987
Luiserebii <luis@serebii.io>
2019-11-05 21:07:16 -0500

    operator* and operator/ working, tests written

--- operator_overloading/string_op/main.cpp
+++ operator_overloading/string_op/main.cpp
@@ -8,20 +8,23 @@ using std::endl;
 using std::string;
 
 ostream& print(const String& sO, const string& name);
+ostream& title(const string& msg);
 
 int main() {
 
+    //operator+
+    title("operator+");
     String s = String("uguu");
     String t = String("owo");
 
     print(s, "s");
     print(t, "t");
 
-    //operator+
     String sum = s + t;
-    cout << "s + t: " << sum.str() << endl; 
+    cout << "s + t: " << sum.str() << endl << endl; 
 
     //operator-
+    title("operator-");
     String f = String("Fly me to the moon");
     String m = String("me to the ");
     
@@ -29,17 +32,30 @@ int main() {
     print(m, "m");
 
     String diff = f - m;
-    cout << "f - m: " << diff.str() << endl;
+    cout << "f - m: " << diff.str() << endl << endl;
 
     //operator*
+    title("operator*");
+    String prod = s * 5;
+    cout << "s * 5: " << prod.str() << endl << endl;
 
     //operator/
+    title("operator/");
+    String quot = prod / 5;
+    cout << "prod / 5: " << quot.str() << endl << endl;
 
     //operator[]  
+    title("operator[]");
+    cout << "s[3]: " << f[3] << endl;
 
 }
 
 ostream& print(const String& sO, const string& name) {
     cout << "String " << name << ": " << sO.str() << endl;
+    return cout;
+}
+
+ostream& title(const string& msg) {
+    cout << msg << endl << "=====================" << endl; 
     return cout;
 }
