b22ae25ad87c84b87c35a17621954d8d9ce7b44b
Luiserebii <luis@serebii.io>
2019-11-07 02:11:33 -0500

    Wrote basic constructor out and push_back... somehow, this doesn't feel all right. I'm missing some big things here, and running into some uncertainties

--- memory/vector/vector.cpp
+++ memory/vector/vector.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <memory>
 
+using std::allocator;
 
 template <class T>
 class Vector {
@@ -9,7 +11,10 @@ class Vector {
         typedef T* iterator;
         typedef const T* const_iterator;
 
+        //Constructors
+        Vector();
 
+        void push_back(const T& val);
 
         iterator begin() { return head; }
         const_iterator begin() const { return head; }
@@ -23,6 +28,34 @@ class Vector {
 
         iterator head;
         iterator tail;
-
+        allocator<T> alloc;
 
 };
+
+template <class T>
+Vector<T>::Vector() {
+    head = 0;
+    tail = 0;
+}
+
+template <class T>
+void Vector<T>::push_back(const T& val) {
+    //Allocate memory for newly added value
+    size_t newSize = size() + 1;
+
+    if(size() != 0) {
+        //Allocate new stuff
+        T* b = alloc.allocate(newSize);
+
+        //Copy stuff over
+        T* e = uninitialized_copy(head, tail, b);
+
+        //Destroy old stuff (don't we have to destroy individually, or deallocation is fine...?)
+        alloc.deallocate(head, size());
+
+        //Set new stuff up
+        head = b;
+        tail = e;
+        
+    }
+}
