640b8352af4b1b156e2d0a29d297d52c99017bc4
Luiserebii <luis@serebii.io>
2019-11-12 20:47:50 -0500

    Setting up new Integer class to overload

---
+++ operator_overloading/integer/integer.h
@@ -0,0 +1,25 @@
+#ifndef GUARD_INTEGER_H
+#define GUARD_INTEGER_H
+
+
+class Integer {
+
+    public:
+        Integer();
+        Integer(int v);
+        //Automatic conversion for doubles
+        Integer(double v);
+        //Construction via strings **MUST** be explicit
+        explicit Integer(const string& s);
+
+    private:
+        int val;
+
+
+
+
+
+}
+
+
+#endif
